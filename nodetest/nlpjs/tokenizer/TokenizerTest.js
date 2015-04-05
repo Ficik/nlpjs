00cc3bcaf9d1f3021c3de5c1e99154e98f134174
Standa Fifik <standa.fifik@gmail.com>
2015-04-05 18:54:21 +0200

    adding simple tokenizer test

---
+++ nodetest/nlpjs/tokenizer/TokenizerTest.js
@@ -0,0 +1,15 @@
+import Tokenizer from '../../../src/nlpjs/tokenizer/Tokenizer.js';
+
+import Document from '../../../src/nlpjs/core/Document.js';
+
+describe('Tokenizer', function(){
+
+    it('should tokenize lorem ipsum', function(){
+        var tokenizer = new Tokenizer();
+        var doc = new Document('Lipsum');
+        doc.text = "Lorem ipsum dolor sit amet";
+        tokenizer.tokenize(doc);
+        doc.annotations.type('word').size.should.be.exactly(5);
+    });
+
+});
